674873c7e99e15f9d8f2a51b525bf3f5b50f1a58
Aina RAZAFIMAHEFA <aina@mobilabsolutions.com>
2012-11-17 20:41:44 +0100

    Composing pure functions to reduce boilerplate code

---
+++ src/chapter/two/Box.scala
@@ -0,0 +1,20 @@
+package chapter.two
+
+case class Box(height: Double, width: Double)
+
+object Box {
+
+  /**
+   * Defines comparison in terms of pure functions
+   * The greaterBy function accepts a pure function (Box => Double) and this function
+   * returned values are used to do the comparison
+   */
+  def greaterBy(x: Box, y: Box, f: Box => Double): Box = if (f(x) > f(y)) x else y
+
+  /** Defines wider to be a comparison on the box width */
+  def wider(x: Box, y: Box): Box = greaterBy(x, y, p => p.width)
+
+  /** Defines wider to be a comparison on the box height */
+  def taller(x: Box, y: Box): Box = greaterBy(x, y, p => p.height)
+
+}
---
+++ src/chapter/two/MyModule.scala
@@ -0,0 +1,14 @@
+package chapter.two
+
+object MyModule {
+
+  def abs(n: Int): Int = if (n < 0) -n else n
+
+  private def formatAbs(x: Int) = {
+    val s = "The absolute value of %d is %d."
+    s.format(x, abs(x))
+  }
+
+  /** Program entry point */
+  def main(args: Array[String]): Unit = println(formatAbs(-42))
+}
