442f1c2b7f6279e2bc500c5337f96214388c14b9
Aina RAZAFIMAHEFA <aina@mobilabsolutions.com>
2012-11-18 16:56:29 +0100

    Composing functions to enhance code reuse and modularity

--- src/chapter/two/FunctionLiterals.scala
+++ src/chapter/two/FunctionLiterals.scala
@@ -1,10 +1,13 @@
 package chapter.two
 
 object FunctionLiterals {
-  
-  val max:(Int, Int) => Int = (a, b) => if(a > b) a else b
-  
-  /** Using the underscore notation to define functions */
+
+  val max: (Int, Int) => Int = (a, b) => if (a > b) a else b
+
+  /**
+   * EXERCICE 1
+   * Using the underscore notation to define functions
+   */
   val lessThan: (Int, Int) => Boolean = _ < _
-  
+
 }
