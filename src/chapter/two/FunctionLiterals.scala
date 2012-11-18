1b1f19ccc6288af8652a0a79d3ace0c9dca8a189
Aina RAZAFIMAHEFA <aina@mobilabsolutions.com>
2012-11-18 16:58:49 +0100

    Refactored FunctionLiterals

--- src/chapter/two/FunctionLiterals.scala
+++ src/chapter/two/FunctionLiterals.scala
@@ -9,5 +9,14 @@ object FunctionLiterals {
    * Using the underscore notation to define functions
    */
   val lessThan: (Int, Int) => Boolean = _ < _
+  
+  /** Defines a function that negates another function */
+  def not(p: Int => Boolean): Int => Boolean = n => !(p(n))
+  def even(n: Int): Boolean = n % 2 == 0
+  def negative(n: Int): Boolean = n < 0
+  
+  /** Defining odd and positive using the not function */
+  val odd = not(even)
+  val positive = not(negative)
 
 }
