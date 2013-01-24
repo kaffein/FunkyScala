252f8a1b9cf9ec5454b8ef2e73406d3d8abbea95
Aina RAZAFIMAHEFA <aina@mobilabsolutions.com>
2013-01-24 15:11:51 +0100

    Solved exercise 26 : implementing size function in Tree

---
+++ src/chapter/three/datastructures/Tree.scala
@@ -0,0 +1,27 @@
+package chapter.three.datastructures
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nasoloaina
+ * Date: 1/24/13
+ * Time: 3:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+sealed trait Tree[+A]
+
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  /**
+   * EXERCISE 26
+   * Counting the number of nodes in a tree
+   */
+  def size[A](t: Tree[A]): Int = t match {
+    case Branch(left, right) => 1 + size(left) + size(right)
+    case Leaf(_) => 1
+  }
+
+
+}
